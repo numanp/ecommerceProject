20d9db37459b200583ea4c4e45e339fa7982fc76
diegofernandezfontana <diegofernandezfontana@gmail.com>
2018-10-31 15:48:49 -0300

    Agregando SideBarContainer y sidebar

---
+++ front/src/containers/SidebarContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import store from '../store';
+import Sidebar from '../components/SideBar';
+
+export default class SidebarContainer extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return <Sidebar />;
+    }
+}
