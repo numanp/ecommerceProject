54b86091933b8fcd06709d603a892b33e418dd29
ferSiri <f_lp_s@hotmail.com>
2018-11-01 10:54:49 -0300

    dos

--- front/src/containers/Main.jsx
+++ front/src/containers/Main.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-
 import NavBar from './NavBar'
 import LandingPage from './LandingPage'
+import Login from '../components/Login'
+import SignUp from '../components/SignUp';
+import axios from 'axios';
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-        }
+        this.sign = this.sign.bind(this);
+        this.logn = this.logn.bind(this);
+        this.logout = this.logout.bind(this);
     }
-    
+    sign(object) {
+        axios.post('api/signup', object)
+    }
+    logn(object) {
+        axios.post('api/login', object).then(res => console.log(res.data))
+    }
+    logout() {
+        axios.post('api/logout')
+            .then(res => console.log(res.data))
+    }
+    componentDidMount() {
+        // axios.get('/api/me')
+        //     .then((user) => { console.log('THIS IS ME ', user) })
+        //     .catch(console.log)
+
+    }
+
     render() {
         return (
             <div>
                 <NavBar />
-                <LandingPage />
+                < Route exact path="/" component={LandingPage} />
+                < Route exact path="/signup" render={() => <SignUp sign={this.sign} />} />
+                < Route exact path="/login" render={() => <Login logout={this.logout} logn={this.logn} />} />
             </div>
         );
     }
