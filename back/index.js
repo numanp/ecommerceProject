41b2b68f6cc925b3c86e57d3966153fce58b391f
orianachv <chacon.oriana.19@gmail.com>
2018-11-01 11:32:57 -0300

    producto

--- back/index.js
+++ back/index.js
@@ -5,18 +5,16 @@ var path = require('path');
 const db = require('./models/db');
 const models = require('./models/index').modelos;
 
-
-db.sync({force: true})
-.then(function () {
-    app.listen('3001', function () {
-        console.log('listening at 3001');
-    });
-})
+db.sync({ force: true }).then(function() {
+  app.listen('3001', function() {
+    console.log('listening at 3001');
+  });
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('../front/dist'));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.resolve('../front/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.resolve('../front/index.html'));
 });
