c513cf2f3cca7bde1ea53afc337d2bba52de26c1
ferSiri <f_lp_s@hotmail.com>
2018-11-06 14:42:09 -0300

    grandes avances en carrito

--- front/src/redux/action-creators/cart.js
+++ front/src/redux/action-creators/cart.js
@@ -1,10 +1,24 @@
 // import axios from 'axios';
-import { ADD_TO_CART } from '../constants';
+import { ADD_TO_CART, REMOVE_FROM_CART, ADD_Q_TO_PRODUCTO, LESS_Q_TO_PRODUCTO } from '../constants';
 import store from '../store'
 
-const addToCart = producto => ({
+export const addToCart = (producto) => ({
   type: ADD_TO_CART,
   payload: producto
+})
+
+export const removeFromCart = productoId => ({
+  type: REMOVE_FROM_CART,
+  payload: productoId
+})
+
+export const addQtoProduct = (productoId) => ({
+  type: ADD_Q_TO_PRODUCTO,
+  payload: productoId
+})
+
+const lessQtoProduct = (productoId) => ({
+  type: LESS_Q_TO_PRODUCTO
 })
 
 
--- front/src/redux/reducers/cart-reducer.js
+++ front/src/redux/reducers/cart-reducer.js
@@ -1,16 +1,38 @@
-import {ADD_TO_CART, REMOVE_FROM_CART} from '../constants';
+import { ADD_TO_CART, REMOVE_FROM_CART, ADD_Q_TO_PRODUCTO, LESS_Q_TO_PRODUCTO } from '../constants';
 
 const cartReducer = (state = [], action) => {
-    switch(action.type) {
+    switch (action.type) {
         case ADD_TO_CART:
-            return [...state, action.payload]
-        // case REMOVE_FROM_CART:
-        //     const firstMatchIndex = state.indexOf(action.payload)
-        //     return state.filter( (item, index) => index !== firstMatchIndex)
-
+            var index = -1
+            for (var i = 0; i < state.length; i++) {
+                if (state[0].id == action.payload.id) {
+                    index = i
+                }
+            }
+            if (index == -1) return [...state, action.payload]
+        case ADD_Q_TO_PRODUCTO:
+            var obj = state.find((i) => i.id == action.payload).q++
+            return Object.assign([], state, { cart: obj });
+        case LESS_Q_TO_PRODUCTO:
+            var obj = state.find((i) => i.id == action.payload).q--
+            return Object.assign([], state, { cart: obj });
+        case REMOVE_FROM_CART:
+            var index = -1
+            for (var i = 0; i < state.length; i++) {
+                if (state[0].id == action.payload) {
+                    index = i
+                }
+            }
+            if (index !== -1) {
+                return state = [
+                    ...state.slice(0, index),
+                    ...state.slice(index + 1)
+                ]
+            }
         default:
             return state
     }
 }
 
 export default cartReducer
+
