c34fb8282fa00a4d08670e0979ff55c64895576d
gonpeche <espeche.g@gmail.com>
2018-11-08 10:59:26 -0300

    mejoras checkout

--- front/src/containers/checkOutContainer.jsx
+++ front/src/containers/checkOutContainer.jsx
@@ -1,82 +1,68 @@
-import React, { Component} from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
 
+class CheckoutContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+    }
+    //MANEJA FORMULARIO PARA AGREGAR PRODUCTO
+
+    render() {
+        return (
+            <div className="container" id="checkOutContaier">
+                <div className="contenedorCheckout">
+                    <div className='col-md-4 IMGCHECKOUT'>
+                     
+                    </div>
+                        <div className='col-md-4 checkoutForm'>
+                            <h2>Total a pagar: 100</h2>
+                            <form>
+                            <div class="form-group">
+                                <label for="email">Nombre de tarjeta</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="tarjeta">Tarjeta</label>
+                                <input type="password" class="form-control" id="tarjeta" placeholder="Password"/>
+                            </div>
+                            <div className="datos">
+                                <div class="form-group">
+                                    <label for="tarjeta">Expiration Date</label>
+                                    <input type="text" class="form-control checkoutExpiracion" id="tarjeta" placeholder="FECHA EXPIRACION"/>
+                                </div>
+                                <div class="form-group cvvcheckout">
+                                <label for="tarjeta">CVV</label>
+                                    <input type="text" class="form-control " id="tarjeta" placeholder="FECHA EXPIRACION"/>
+                                </div>
+                            </div>
+                    
+                            <button type="submit" class="btn btn-success btn-block btn-lg">Realizar pago</button>
+                        
+                            </form> 
+                        </div>
+                </div>
+            </div>
+        )
+    }
+}
 function mapStateToProps(state) {
-  return {
-  }
+    return {
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-  }
+    return {
+        addProduct: function (producto) {
+            dispatch(addProduct(producto))
+        },
+        fetchCategorys: function (categorias) {
+            dispatch(fetchCategorys(categorias))
+        },
+    }
 }
 
-var arregloCompras = [
-  {
-      idVenta: 1,
-      productos: [10,20,30],
-      fechaVenta: '10:25PM',
-      status: 'Completado',
-      linkProducto: '#'
-  },{
-      idVenta: 3,
-      productos: [10,20,30],
-      fechaVenta: '10:25PM',
-      status: 'incompleto',
-      linkProducto: '#'
-  },
-  ,{
-      idVenta: 4,
-      productos: [10,20,30],
-      fechaVenta: '10:25PM',
-      status: 'incompleto',
-      linkProducto: '#'
-  },
-]
-class CheckoutContainer extends Component {
-  render() {
-    return (
-      <div  className="container-fluid container" id="">
-      <h1>Manejo de ordenes</h1>
-      
-      {
-          //DROPDOWN PARA FILTRAR LAS CATEGORIAS
-      }
-      <div class="btn-group">
-          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Filtrar por Status <span class="caret"></span>
-           </button>
-              <ul class="dropdown-menu">
-                  <li><a href="#">CREADO</a></li>
-                  <li><a href="#">PROCESADO</a></li>
-                  <li><a href="#">CANCELADO</a></li>
-                  <li><a href="#">COMPLETADO</a></li>
-             </ul>
-      </div>
-  
-  
-      { //MAPEO POR EL ARREGLO DE COMPRAS
-          arregloCompras.map(compra => (
-              <div className="profile_compra"  key={compra.idVenta}>
-                  <a href={compra.linkProducto}>
-                      <img src="https://cdn.jysk.es/media/catalog/product/cache/9/thumbnail/960x/163b81649b7ef7bc8a00b0066e59ae0a/u/n/unbenannt-1_947.jpg" alt=""/>
-                  </a>
-                  <p>INFO DE LA VENTA.</p>
-                  <p>ID VENTA: {compra.idVenta}</p>
-                  <p>Fecha: {compra.fechaVenta}</p>
-                  <p>Status: {compra.status}</p>    
-                  <p class="profile_celeste">Ver Detalle (OTRO COMPONENTE QUE MUESTRE TODOS LOS PRODUCTOS COMPRADOS EN LA VENTA)</p>
-                  <button class="btn btn-primary">Cambiar el Status</button>
-              </div>
-          ))
-      }
-  
-  </div>
-    )
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CheckoutContainer)
 
 
-
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutContainer);
